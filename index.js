37d1ebce3e1b46a26b37c9f35eea8708e4bd82f5
Anastasia Polivoda <polivoda.a@zovplita.by>
2022-05-09 22:51:40 +0300

    Updates

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ window.addEventListener('load', () => {
       document.body.classList.add('loaded');
       document.body.classList.remove('loaded_hiding');
     }, 300);
-  }, 1200);
+  }, 2300);
 });
 
 const shiftLeft = 'ShiftLeft';
